4c92163399f8e64da3a4a68a85ad16caebe0fd14
Ong Zheng Kai <zhengkai@notdns.me>
2026-09-24 12:38:47 +0800

    feat: try 1

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -1,70 +1,70 @@
 import java.util.*;
 
 public class SinglyLinkedList<E extends Comparable<E>> {
+
     private Node<E> head = null;
     private Node<E> tail = null;
     private int size = 0;
 
     private static class Node<E> {
+
         private E element;
         private Node<E> next;
-    
-        public Node(E e, Node<E> n){
+
+        public Node(E e, Node<E> n) {
             element = e;
             next = n;
         }
-    
-        public E getElement(){
+
+        public E getElement() {
             return element;
         }
-    
-        public Node<E> getNext(){
+
+        public Node<E> getNext() {
             return next;
         }
-    
-        public void setNext(Node<E> n){
+
+        public void setNext(Node<E> n) {
             next = n;
         }
     }
 
-    public SinglyLinkedList(){
+    public SinglyLinkedList() {}
 
-    }
-
-    public int size(){
+    public int size() {
         return size;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return size == 0;
     }
 
-    public E first(){
-        if (isEmpty()){
+    public E first() {
+        if (isEmpty()) {
             return null;
-        } 
+        }
         return head.getElement();
     }
 
-    public E last(){
-        if (isEmpty()){
+    public E last() {
+        if (isEmpty()) {
             return null;
         }
         return tail.getElement();
     }
 
-    public void addFirst(E e){
+    public void addFirst(E e) {
         head = new Node<>(e, head);
 
-        if (isEmpty()){
+        if (isEmpty()) {
             tail = head;
         }
         size++;
     }
 
-    public void addLast(E e){
+    public void addLast(E e) {
         Node<E> newest = new Node<>(e, null);
-        if (isEmpty()){
+        if (isEmpty()) {
             head = newest;
         } else {
             tail.setNext(newest);
@@ -73,8 +73,8 @@ public class SinglyLinkedList<E extends Comparable<E>> {
         size++;
     }
 
-    public E removeFirst(){
-        if (isEmpty()){
+    public E removeFirst() {
+        if (isEmpty()) {
             return null;
         }
 
@@ -82,13 +82,13 @@ public class SinglyLinkedList<E extends Comparable<E>> {
         head = head.getNext();
         size--;
 
-        if (isEmpty()){
+        if (isEmpty()) {
             tail = null;
         }
         return answer;
     }
 
-    public String toString(){
+    public String toString() {
         StringBuilder sb = new StringBuilder();
         Node<E> current = head;
         while (current != null) {
@@ -99,11 +99,43 @@ public class SinglyLinkedList<E extends Comparable<E>> {
         return sb.toString();
     }
 
+    void reposition(List<Node<E>> emulatedList, Node<E> min, Node<E> max) {}
+
     // write your codes here
-    public void swap(){
-        
+    public void swap() {
+        List<Node<E>> sortedList = new ArrayList<>();
+        List<Node<E>> emulatedList = new ArrayList<>();
+        Node<E> current = head;
 
+        while (current != null) {
+            sortedList.add(current);
+            emulatedList.add(current);
+            current = current.getNext();
+        }
+
+        sortedList.sort((v1, v2) -> v1.getElement().compareTo(v2.getElement()));
+
+        int total = this.size / 2;
+        for (int i = 0; i < total; i++) {
+            Node<E> min = sortedList.getFirst();
+            Node<E> max = sortedList.getLast();
+
+            int minOldIdx = emulatedList.indexOf(min);
+            int maxOldIdx = emulatedList.indexOf(max);
+
+            emulatedList.set(minOldIdx, max);
+            emulatedList.set(maxOldIdx, min);
+
+            sortedList.removeFirst();
+            sortedList.removeLast();
+        }
+
+        for (int i = 0; i < this.size - 1; i++) {
+            emulatedList.get(i).setNext(emulatedList.get(i + 1));
+        }
+
+        this.head = emulatedList.getFirst();
+        this.tail = emulatedList.getLast();
+        emulatedList.getLast().setNext(null);
     }
-   
 }
-
